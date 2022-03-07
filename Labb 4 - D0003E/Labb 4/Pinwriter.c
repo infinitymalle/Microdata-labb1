73be27c4847382296402191cc49761df67d6ff23
eddisono <eddison97@hotmail.com>
2022-03-07 23:54:07 +0100

    Update Pinwriter.c

--- Labb 4 - D0003E/Labb 4/Pinwriter.c
+++ Labb 4 - D0003E/Labb 4/Pinwriter.c
@@ -1,7 +1,16 @@
-//#include <avr/io.h>
-//#include <avr/interrupt.h>
-//#include <stdbool.h>
-//#include "pinwriter.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdbool.h>
+#include "pinwriter.h"
 //
 //
 //
+void write(Writer *self){
+          *USCK = freq;
+          *DO = freq;
+}
+
+// https://dtsheet.com/doc/1815614/view-detail-for-avr-butterfly-evaluation-kit---user-guide
+//Pin 1 (USCK/SCL) PORTE bit 4
+//Pin 3 (DO)       PORTE bit 6
+//Pin 4 (GND)
